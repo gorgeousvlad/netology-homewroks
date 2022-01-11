6a6278339d5c6c62a3d80470246373d20436e3cc
VladVlad <vladvlad@yandex-team.ru>
2022-01-11 21:20:16 +0300

    add routes

---
+++ 2-library/routes/books.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const fileMiddleware = require('../middleware/file');
+
+const {Book, filterReqFields} = require('../models/Book');
+const store = {
+    books: [],
+};
+
+[1, 2, 3].map(el => {
+    store.books.push(new Book({title: `book ${el}`, description: `book ${el} description`}))
+});
+
+
+router.get('/', (req, res) => {
+    const {books} = store;
+    res.json(books);
+});
+
+router.get('/:id', (req, res) => {
+    const {books} = store;
+    const {id} = req.params;
+    const idx = books.findIndex(el => el.id === id);
+
+    if (idx !== -1) {
+        res.json(books[idx]);
+    } else {
+        res.status(404);
+        res.json(`book ${id} | not found`);
+    }
+});
+
+router.post('/', (req, res) => {
+    const {books} = store;
+    const book = new Book(filterReqFields(req.body));
+    books.push(book);
+
+    res.status(201);
+    res.json(book);
+});
+
+router.put('/:id', (req, res) => {
+    const {books} = store;
+    const {id} = req.params;
+    const idx = books.findIndex(el => el.id === id);
+
+    if (idx !== -1) {
+        books[idx] = {
+            ...books[idx],
+            ...filterReqFields(req.body || {})
+        };
+
+        res.json(books[idx]);
+    } else {
+        res.status(404);
+        res.json(`books ${id}| not found`);
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    const {books} = store;
+    const {id} = req.params;
+    const idx = books.findIndex(el => el.id === id);
+
+    if (idx !== -1) {
+        books.splice(idx, 1);
+        res.json(true);
+    }  else {
+        res.status(404);
+        res.json(`book ${id}| not found`);
+    }
+});
+
+// загрузка файлов
+router.post('/upload-img', fileMiddleware.single('cover-img'), (req, res) => {
+    if (req.file) {
+        const {path} = req.file;
+        console.log(path);
+
+        res.json(path);
+    } else {
+        res.json(null);
+    }
+});
+
+router.get('/:id/download-img', (req, res) => {
+    res.download(__dirname+'/../public/img/2020-12-07-cover.png', 'cover.png', err=>{
+        if (err){
+            res.status(404).json();
+        }
+    });
+});
+
+module.exports = router;
