938fda553ebf4997323d0b905835282288bbdd20
VladVlad <vladvlad@yandex-team.ru>
2022-01-16 12:18:35 +0300

    add upload file extendsions filter

--- 2-library/middleware/file.js
+++ 2-library/middleware/file.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const multer = require('multer');
 
 const PUBLIC_PATH =  '/../public/books';
+const ALLOWED_EXTENDSIONS = ['.png','.jpg','.jpeg', '.txt', '.rtf','.fb2'];
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -11,19 +13,16 @@ var storage = multer.diskStorage({
   }
 })
 
-// const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
-
-// const fileFilter = (req, file, cb) => {
-//   if (allowedTypes.includes(file.mimetype)) {
-//     cb(null, true)
-//   } else {
-//     cb(null, false)
-//   }
-// };
-
 var upload = multer({
     storage: storage,
-    // fileFilter,
+    fileFilter: function (req, file, callback) {
+      var ext = path.extname(file.originalname);
+
+      if(!ALLOWED_EXTENDSIONS.includes(ext)) {
+          return callback(new Error('Only images are allowed'))
+      }
+      callback(null, true)
+  },
     onFileUploadStart: function (file) {
       console.log(file.originalname + ' is starting ...')
     },
