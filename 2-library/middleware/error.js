c582026719433d136fd73b96cdc5068a78b54c0b
VladVlad <vladvlad@yandex-team.ru>
2022-01-22 14:08:16 +0300

    add error template

--- 2-library/middleware/error.js
+++ 2-library/middleware/error.js
@@ -1,5 +1,6 @@
 module.exports = (req, res) => {
     res.status(404);
-    const content = '404 | not found';
-    res.send(content);
+    // const content = '404 | not found';
+    // res.send(content);
+    res.render("error/404",{title: "Page not found"})
 };
